90e5ae6a136602471f6335e245e92b1f3ff8087b
Andrés Tinoco <andrestinoco06@outlook.com>
2018-02-19 19:34:31 -0500

    Centenario.java
    Imprime numero del 1 al 100

---
+++ Centenario.java
@@ -0,0 +1,9 @@
+public class Centenario {
+    public static void main(String[] args) {
+        int num=1;
+        while (num<=100){
+            System.out.println(+num);
+            num++;
+        }
+    }
+}
